c38233552f611a785501b7403258b11867a4f075
amon <amon@nandynarwhals.org>
2019-05-31 17:33:01 -0700

    More parsing definitions.

--- findsecbugs_parser/fsb.py
+++ findsecbugs_parser/fsb.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import xml.etree.ElementTree as ET
+from collections import OrderedDict
+
 
 class ReportParser:
 
@@ -9,33 +12,33 @@ class ReportParser:
         self.initialise(root)
 
     def initialise(self, root):
-        bugreport_proto = self.init_attribs(root.attrib)
+        bugcollection_proto = self.init_attribs(root.attrib)
 
         project = self.init_project(root)
-        bugreport_proto.set_project(project)
+        bugcollection_proto.set_project(project)
 
         buginstances = self.init_buginstances(root)
-        bugreport_proto.set_buginstances(buginstances)
+        bugcollection_proto.set_buginstances(buginstances)
 
         errors = self.init_errors(root)
-        bugreport_proto.set_errors(errors)
+        bugcollection_proto.set_errors(errors)
 
         bugssummary = self.init_findbugssummary(root)
-        bugreport_proto.set_findbugssummary(bugssummary)
+        bugcollection_proto.set_findbugssummary(bugssummary)
 
         classfeatures = self.init_classfeatures(root)
-        bugreport_proto.set_classfeatures(classfeatures)
+        bugcollection_proto.set_classfeatures(classfeatures)
 
         history = self.init_history(root)
-        bugreport_proto.set_history(history)
+        bugcollection_proto.set_history(history)
 
-        self.bugreport = bugreport_proto
+        self.bugcollection = bugcollection_proto
 
     def init_attribs(self, attribs):
         version = attribs.get('version', 'N/A')
-        sequence = attribs.get('sequence', 'N/A')
-        timestamp = attribs.get('timestamp', 'N/A')
-        analysisTimestamp = attribs.get('analysisTimestamp', 'N/A')
+        sequence = int(attribs.get('sequence', -1))
+        timestamp = int(attribs.get('timestamp', -1))
+        analysisTimestamp = int(attribs.get('analysisTimestamp', -1))
         release = attribs.get('release', 'N/A')
         bc = BugCollection(version, sequence, timestamp, analysisTimestamp, release)
         return bc
@@ -123,6 +126,7 @@ class BugCollection:
     def set_history(self, history):
         self.history = history
 
+
 class Project:
 
     def __init__(self, project_root):
@@ -132,27 +136,167 @@ class Project:
         plugin_element = project_root.find('Plugin')
         self.plugin = plugin_element.attrib
 
+
 class BugInstance:
 
     def __init__(self, bi_root):
+        attribs = bi_root.attrib
+        self.type = attribs.get('type', 'N/A')
+        self.priority = attribs.get('priority', 'N/A')
+        self.rank = attribs.get('rank', 'N/A')
+        self.abbrev = attribs.get('abbrev', 'N/A')
+        self.category = attribs.get('category', 'N/A')
+
+        self.strings = OrderedDict()
+        string_elements = bi_root.findall('String')
+        for i in string_elements:
+            role = i.attrib.get('role')
+            value = i.attrib.get('value')
+            self.strings[role] = value
+
+        bug_class_element = bi_root.find('Class')
+        self.bug_class = BugClass(bug_class_element)
+
+        bug_method_element = bi_root.find('Method')
+        self.bug_method = BugMethod(bug_method_element)
+
+        self.sourcelines = SourceLines(bi_root)
+
+
+class BugClass:
+
+    def __init__(self, bc_root):
         pass
+
+
+class BugMethod:
+
+    def __init__(self, bm_root):
+        pass
+
+
+class SourceLines:
+
+    def __init__(self, root):
+        sourceline_elements = root.findall('SourceLine')
+        self.lines = []
+        for i in sourceline_elements:
+            self.lines.append(SourceLine(i))
+
+
+class SourceLine:
+
+    def __init__(self, sl_root):
+        attribs = sl_root.attrib
+        self.classname = attribs.get('classname', 'N/A')
+        self.start = int(attribs.get('start', -1))
+        self.end = int(attribs.get('end', -1))
+        self.startBytecode = int(attribs.get('startBytecode', -1))
+        self.endBytecode = int(attribs.get('endBytecode', -1))
+        self.sourcefile = attribs.get('sourcefile', 'N/A')
+        self.sourcepath = attribs.get('sourcepath', 'N/A')
+
 
 class Errors:
 
     def __init__(self, errors_root):
-        pass
+        attribs = errors_root.attrib
+        self.num_errors = int(attribs.get('errors', 0))
+        self.num_missingclasses = int(attribs.get('missingClasses', 0))
+        self.errors = []
+        for i in errors_root.findall('Error'):
+            pass  # TODO: Populate the error list.
+        self.missingclasses = []
+        for i in errors_root.findall('MissingClass'):
+            self.missingclasses.append(i.text)
+
 
 class FindBugsSummary:
 
     def __init__(self, bs_root):
-        pass
+        attribs = bs_root.attrib
+        self.timestamp = attribs.get('timestamp', 'N/A')
+        self.total_classes = attribs.get('total_classes', 'N/A')
+        self.referenced_classes = attribs.get('referenced_classes', 'N/A')
+        self.total_bugs = attribs.get('total_bugs', 'N/A')
+        self.total_size = attribs.get('total_size', 'N/A')
+        self.num_packages = attribs.get('num_packages', 'N/A')
+        self.java_version = attribs.get('java_version', 'N/A')
+        self.vm_version = attribs.get('vm_version', 'N/A')
+        self.cpu_seconds = attribs.get('cpu_seconds', 'N/A')
+        self.clock_seconds = attribs.get('clock_seconds', 'N/A')
+        self.peak_mbytes = attribs.get('peak_mbytes', 'N/A')
+        self.alloc_mbytes = attribs.get('alloc_mbytes', 'N/A')
+        self.gc_seconds = attribs.get('gc_seconds', 'N/A')
+        self.priority_2 = attribs.get('priority_2', 'N/A')
+        self.priority_1 = attribs.get('priority_1', 'N/A')
+
+        self.packagestats = []
+        packagestat_elements = bs_root.findall('PackageStats')
+        for i in packagestat_elements:
+            self.packagestats.append(PackageStats(i))
+
+        fbp_element = bs_root.find('FindBugsProfile')
+        self.findbugsprofile = FindBugsProfile(fbp_element)
+
+
+class PackageStats:
+
+    def __init__(self, ps_root):
+        attribs = ps_root.attrib
+        self.package = attribs.get('package', 'N/A')
+        self.total_bugs = attribs.get('total_bugs', 'N/A')
+        self.total_types = attribs.get('total_types', 'N/A')
+        self.total_size = attribs.get('total_size', 'N/A')
+
+        self.classstats = []
+        classstats_elements = ps_root.findall('ClassStats')
+        for i in classstats_elements:
+            self.classstats.append(ClassStats(i))
+
+
+class ClassStats:
+
+    def __init__(self, cs_root):
+        attribs = cs_root.attrib
+        self.class_name = attribs.get('class', 'N/A')
+        self.source_file = attribs.get('sourceFile', 'N/A')
+        self.interface = attribs.get('interface', 'N/A')
+        self.size = int(attribs.get('size', -1))
+        self.bugs = int(attribs.get('bugs', -1))
+
+
+class FindBugsProfile:
+
+    def __init__(self, profile_root):
+        self.classprofiles = []
+        classprofile_elements = profile_root.findall('ClassProfile')
+        for i in classprofile_elements:
+            self.classprofiles.append(ClassProfile(i))
+
+
+class ClassProfile:
+
+    def __init__(self, cp_root):
+        attribs = cp_root.attrib
+        self.name = attribs.get('name', 'N/A')
+        self.totalMilliseconds = int(attribs.get('totalMilliseconds', -1))
+        self.invocations = int(attribs.get('invocations', -1))
+        self.avgMicrosecondsPerInvocation = int(attribs.get('avgMicrosecondsPerInvocation', -1))
+        self.maxMicrosecondsPerInvocation = int(attribs.get('maxMicrosecondsPerInvocation', -1))
+        self.standardDeviationMicrosecondsPerInvocation = int(attribs.get(
+            'standardDeviationMicrosecondsPerInvocation', -1))
+
 
 class ClassFeatures:
 
     def __init__(self, cf_root):
+        # TODO: Not implemented yet since the samples leave the property empty.
         pass
+
 
 class History:
 
     def __init__(self, history_root):
+        # TODO: Not implemented yet since the samples leave the property empty.
         pass
--- tests/test_fsb.py
+++ tests/test_fsb.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-from findsecbugs_parser.fsb import ReportParser
-from distutils import dir_util
-from pytest import fixture
+
 import os
+from distutils import dir_util
+
+from findsecbugs_parser.fsb import (BugCollection, ClassFeatures, Errors,
+                                    FindBugsProfile, FindBugsSummary, History,
+                                    PackageStats, Project, ReportParser)
+from pytest import fixture
 
 
 @fixture
@@ -22,32 +27,89 @@ def datadir(tmpdir, request):
 
     return tmpdir
 
+
 @fixture
 def samplexml(datadir):
     sample_report_fd = datadir.join('sample_report.xml')
     return sample_report_fd.read()
 
-@fixture
-def samplehtml(datadir):
-    sample_report_fd = datadir.join('sample_report.html')
-    return sample_report_fd.read()
+# @fixture
+# def samplehtml(datadir):
+#    sample_report_fd = datadir.join('sample_report.html')
+#    return sample_report_fd.read()
+
 
 @fixture
 def parsedreport(samplexml):
     return ReportParser.parse_string(samplexml)
+
 
 @fixture
 def parsedreport_path(datadir):
     sample_report_fd = datadir.join('sample_report.xml')
     return str(sample_report_fd)
 
+
 def test_reportparser_fromstring(parsedreport):
     assert parsedreport is not None
+
 
 def test_reportparser_fromfile(parsedreport_path):
     frompath = ReportParser.parse_file(parsedreport_path)
     assert frompath is not None
 
-def test_reportparser(parsedreport):
+
+def test_reportparser_instantiation(parsedreport):
     obj = parsedreport
     assert obj is not None
+
+
+def test_reportparser_parsed_bugcollection(parsedreport):
+    assert parsedreport.bugcollection is not None
+    assert type(parsedreport.bugcollection) is BugCollection
+    bc = parsedreport.bugcollection
+    assert bc.version == '3.1.5'
+    assert bc.sequence == 0
+    assert bc.timestamp == 1559244040000
+    assert bc.analysisTimestamp == 1559244097257
+    assert bc.release == ''
+
+
+def test_reportparser_parsed_project(parsedreport):
+    assert parsedreport.bugcollection.project is not None
+    assert type(parsedreport.bugcollection.project) is Project
+    project = parsedreport.bugcollection.project
+    assert project.project_name == ''
+    assert project.jar == '/Users/amon/sproink/guacamole/guacamole-1.0.0.war'
+    assert type(project.plugin) is dict
+    assert len(project.plugin.keys()) == 2
+    assert project.plugin['id'] == 'com.h3xstream.findsecbugs'
+    assert project.plugin['enabled'] == 'true'
+
+
+def test_reportparser_parsed_errors(parsedreport):
+    assert parsedreport.bugcollection.errors is not None
+    assert type(parsedreport.bugcollection.errors) is Errors
+    errors = parsedreport.bugcollection.errors
+    assert errors.num_errors == 0
+
+
+def test_reportparser_parsed_findbugssummary(parsedreport):
+    assert parsedreport.bugcollection.findbugssummary is not None
+    assert type(parsedreport.bugcollection.findbugssummary) is FindBugsSummary
+    bs = parsedreport.bugcollection.findbugssummary
+    assert len(bs.packagestats) > 0
+    assert False not in [type(i) == PackageStats for i in bs.packagestats]
+    assert type(bs.findbugsprofile) == FindBugsProfile
+
+
+def test_reportparser_parsed_classfeatures(parsedreport):
+    assert parsedreport.bugcollection.classfeatures is not None
+    assert type(parsedreport.bugcollection.classfeatures) is ClassFeatures
+    # errors = parsedreport.bugcollection.classfeatures
+
+
+def test_reportparser_parsed_history(parsedreport):
+    assert parsedreport.bugcollection.history is not None
+    assert type(parsedreport.bugcollection.history) is History
+    # errors = parsedreport.bugcollection.history
